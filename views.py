8014c37857334d89aa21b35fb1f2733e0a758b89
Deepak Singh <deepakthakur755@gmail.com>
2020-01-15 15:53:31 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,22 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-#
-# from chatterbot import ChatBot
-# from chatterbot.trainers import ListTrainer
-#
-# #creating a new chatbot
-# chatbot=ChatBot("Deepak")
-# trainer=ListTrainer(chatbot)
-# trainer.train(['hi,can i help you find a plan',"sure I'd love to find you a plan","Your plan have been selected"])
-#
-# # getting a response from the chatbot
-# response=chatbot.get_response("I want a plan")
-# print(response)
-
-
-
-from django.shortcuts import render
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
@@ -44,11 +26,8 @@ def jarvis_chat_bot(request):
 from django.http import HttpResponse
 def getBotResponse(request):
     if request.method=='GET':
-        print("request.GET",request.GET)
         userText = request.GET['msg']
-        print("userText",userText)
         response=str(english_bot.get_response(userText))
-        print("response : ",response)
         return HttpResponse(response)
-        return response
+ 
 
